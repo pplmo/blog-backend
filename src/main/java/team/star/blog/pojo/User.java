f65c267cd34155e06f091e46a72b914ba289039d
mystic <215104920@qq.com>
2019-12-17 14:37:16 +0800

    connect to database

--- src/main/java/team/star/blog/pojo/User.java
+++ src/main/java/team/star/blog/pojo/User.java
@@ -1,8 +1,32 @@
 package team.star.blog.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.Date;
+
 /**
  * @author mystic
  * @date 2019-12-15 19:43
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
 public class User {
+    @Id
+    private int id;
+    private String name;
+    private String password;
+    private String mail;
+    private String url;
+    private String screenName;
+    private Date created;
+    private Date activated;
+    private Date logged;
+    private String group;
+    private String authCode;
 }
--- src/main/java/team/star/blog/repository/UserRepository.java
+++ src/main/java/team/star/blog/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package team.star.blog.repository;
 
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
+import team.star.blog.pojo.User;
+
 /**
  * @author mystic
  */
-public interface UserRepository {
+@Repository
+public interface UserRepository extends R2dbcRepository<User, Integer> {
 }
