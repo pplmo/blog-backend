34dbb91b786f0c03ce905e517d8ac3263a19d0b1
mystic <215104920@qq.com>
2019-12-20 17:21:06 +0800

    add some annotation

--- src/main/java/team/star/blog/pojo/Metadata.java
+++ src/main/java/team/star/blog/pojo/Metadata.java
@@ -1,8 +1,15 @@
 package team.star.blog.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author mystic
  * @date 2019-12-15 19:45
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Metadata {
 }
