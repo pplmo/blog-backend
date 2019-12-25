d58741801557def013dd04ef8fba44c2b3735188
mystic <215104920@qq.com>
2019-12-25 19:30:33 +0800

    add @Table

--- src/main/java/team/star/blog/pojo/Option.java
+++ src/main/java/team/star/blog/pojo/Option.java
@@ -1,12 +1,14 @@
 package team.star.blog.pojo;
 
 import lombok.Data;
+import org.springframework.data.relational.core.mapping.Table;
 
 /**
  * @author mystic
  * @date 2019-12-15 19:44
  */
 @Data
+@Table("t_option")
 public class Option {
     private String name;
     private int userId;
--- src/main/java/team/star/blog/pojo/Relationship.java
+++ src/main/java/team/star/blog/pojo/Relationship.java
@@ -1,11 +1,13 @@
 package team.star.blog.pojo;
 
 import lombok.Data;
+import org.springframework.data.relational.core.mapping.Table;
 
 /**
  * @author mystic
  * @date 2019-12-15 19:44
  */
 @Data
+@Table("t_relationship")
 public class Relationship {
 }
