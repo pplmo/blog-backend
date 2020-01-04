db1337fc48d603949ae3b429a30a51727a1ff9da
mystic <215104920@qq.com>
2020-01-04 20:03:51 +0800

    remove middle table-relationship

--- src/main/java/team/star/blog/service/RelationshipService.java
+++
@@ -1,9 +0,0 @@
-package team.star.blog.service;
-
-import team.star.blog.pojo.Relationship;
-
-/**
- * @author mystic
- */
-public interface RelationshipService extends R2dbcService<Relationship, Integer> {
-}
