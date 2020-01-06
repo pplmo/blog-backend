b4f840892eee3b6e38a211cead4179ad3e65082e
mystic <215104920@qq.com>
2020-01-06 17:55:39 +0800

    run test successfully

--- src/test/java/team/star/blog/controller/UserControllerTests.java
+++ src/test/java/team/star/blog/controller/UserControllerTests.java
@@ -2,9 +2,12 @@ package team.star.blog.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import team.star.blog.pojo.User;
 import team.star.blog.service.UserService;
@@ -18,7 +21,7 @@ import java.util.List;
 public class UserControllerTests {
     @Autowired
     private WebTestClient client;
-    @Autowired
+    @MockBean
     private UserService userService;
 
     @BeforeEach
@@ -27,13 +30,15 @@ public class UserControllerTests {
         User u2 = new User();
         u1.setId(1);
         u2.setId(2);
-        Mono.when(userService.findAll()).thenReturn(List.of(u1, u2)).block();
+        u1.setName("cc");
+        Mockito.when(userService.findAll()).thenReturn(Flux.fromIterable(List.of(u1, u2)));
+        Mockito.when(userService.findById(Mockito.anyInt())).thenReturn(Mono.just(u1));
     }
 
     @Test
-    void getAllUsers() {
-        client.get().uri("/user/").exchange()
+    void getUserById() {
+        client.get().uri("/user/1").exchange()
                 .expectStatus().isOk()
-                .expectBodyList(User.class).hasSize(2);
+                .expectBody(User.class);
     }
 }
