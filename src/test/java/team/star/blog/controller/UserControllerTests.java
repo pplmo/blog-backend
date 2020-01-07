1539a7b8f33735a4530a5adfe9c55750062a3c76
mystic <215104920@qq.com>
2020-01-07 13:08:49 +0800

    run test successfully

--- src/test/java/team/star/blog/controller/UserControllerTests.java
+++ src/test/java/team/star/blog/controller/UserControllerTests.java
@@ -2,10 +2,14 @@ package team.star.blog.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -14,31 +18,43 @@ import team.star.blog.service.UserService;
 
 import java.util.List;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
+
 /**
  * @author mystic
  */
 @WebFluxTest(UserController.class)
+@ExtendWith(RestDocumentationExtension.class)
 public class UserControllerTests {
     @Autowired
+    private ApplicationContext context;
     private WebTestClient client;
     @MockBean
     private UserService userService;
 
     @BeforeEach
-    void setUp() {
+    void setUp(RestDocumentationContextProvider provider) {
         User u1 = new User();
         User u2 = new User();
         u1.setId(1);
         u2.setId(2);
         u1.setName("cc");
-        Mockito.when(userService.findAll()).thenReturn(Flux.fromIterable(List.of(u1, u2)));
-        Mockito.when(userService.findById(Mockito.anyInt())).thenReturn(Mono.just(u1));
+
+        client = WebTestClient.bindToApplicationContext(context)
+                .configureClient()
+                .filter(documentationConfiguration(provider))
+                .build();
+        when(userService.findAll()).thenReturn(Flux.fromIterable(List.of(u1, u2)));
+        when(userService.findById(Mockito.anyInt())).thenReturn(Mono.just(u1));
     }
 
     @Test
-    void getUserById() {
+    void findUserById() {
         client.get().uri("/user/1").exchange()
                 .expectStatus().isOk()
-                .expectBody(User.class);
+                .expectBody(User.class)
+                .consumeWith(document("findUserById"));
     }
 }
