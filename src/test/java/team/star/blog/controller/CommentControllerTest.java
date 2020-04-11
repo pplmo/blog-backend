f686ae4bba4d1bdba0a133c5965aca2cd7c6b848
mystic <215104920@qq.com>
2020-04-11 22:23:58 +0800

    add ut for docs

--- src/test/java/team/star/blog/controller/CommentControllerTest.java
+++ src/test/java/team/star/blog/controller/CommentControllerTest.java
@@ -3,16 +3,34 @@ package team.star.blog.controller;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import team.star.blog.service.CommentService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author mystic
  */
+@WebFluxTest(CommentController.class)
+@ExtendWith(RestDocumentationExtension.class)
 class CommentControllerTest {
 
+    @Autowired
+    private ApplicationContext context;
+    private WebTestClient client;
+
+    @MockBean
+    private CommentService commentService;
+
     @BeforeEach
-    void setUp() {
+    void setUp(RestDocumentationContextProvider provider) {
     }
 
     @AfterEach
